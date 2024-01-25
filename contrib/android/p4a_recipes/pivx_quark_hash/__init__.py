3625867544ad7f100585541b2fb4f51f7c8654c9
Dhop14 <douglas@dimecoinnetwork.com>
2024-01-25 09:27:49 -0500

    android-build: update hashing module link

--- contrib/android/p4a_recipes/pivx_quark_hash/__init__.py
+++ contrib/android/p4a_recipes/pivx_quark_hash/__init__.py
@@ -3,11 +3,12 @@ from pythonforandroid.recipe import CythonRecipe
 
 class PivxQuarkHashRecipe(CythonRecipe):
 
-    url = ('https://files.pythonhosted.org/packages/'
-           '42/d0/c231b69d4deda7c8f3513f92042488a043e83a443f3b878011a3fe5af870/pivx_quark_hash-{version}.tar.gz')
-    sha256sum = '4ccaf9567e1fdb2ff0bf5ffaae038dfc45942daa6b7b1a3a72a34d22ecae2171'
+    url = ('https://github.com/dime-coin/'
+           'dime_quark_hash/archive/refs/tags/{version}.tar.gz')
+    sha256sum = '89e591df61d861f5d557b61e66f57fda13034bcda7bbc505981fb9fd431ee5bb'
     version = '1.2'
     depends = ['python3', 'wheel', 'setuptools']
 
 
 recipe = PivxQuarkHashRecipe()
+
