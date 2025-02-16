734e84e277c56098bdd4ce3e10ff0ec08d4bf717
prettyflyforabeeguy <75382474+prettyflyforabeeguy@users.noreply.github.com>
2025-02-16 15:08:31 -0800

    Bugfix for Requested Amount in receive tab.

--- electrum/invoices.py
+++ electrum/invoices.py
@@ -180,7 +180,7 @@ class BaseInvoice(StoredObject):
         amount_msat = self.amount_msat
         if amount_msat in [None, "!"]:
             return amount_msat
-        return int(amount_msat // 1000)
+        return int(amount_msat) #This was causing the issue of not being able to send the correct amount of Dimecoin.  It was amount_msat // 1000.
 
     def set_amount_msat(self, amount_msat: Union[int, str]) -> None:
         """The GUI uses this to fill the amount for a zero-amount invoice."""
@@ -189,7 +189,7 @@ class BaseInvoice(StoredObject):
         else:
             assert isinstance(amount_msat, int), f"{amount_msat=!r}"
             assert amount_msat >= 0, amount_msat
-            amount_sat = (amount_msat // 1000) + int(amount_msat % 1000 > 0)  # round up
+            amount_sat = (amount_msat) + int(amount_msat % 1000 > 0)  # round up,  Removing the amount_msat division, it was // 1000
         if outputs := self.outputs:
             assert len(self.outputs) == 1, len(self.outputs)
             self.outputs = [PartialTxOutput(scriptpubkey=outputs[0].scriptpubkey, value=amount_sat)]
